00b98c956fb1009c0dc05883264e208a5d608d93
Sarper <82522275+sarok01@users.noreply.github.com>
2024-11-27 15:43:06 +0100

    added link for TCGA project catalogue

--- scripts/tcga_mafbinary.R
+++ scripts/tcga_mafbinary.R
@@ -6,43 +6,49 @@ library(SummarizedExperiment)
 
 
 # Set directory
-setwd("D:/R_projects/mutual_exc/tcga_data/")
+setwd("D:/GitHub/sarok01/tcga_data/")
 source(file = "scripts/maf2matrix.R")
 
 # TCGA
-TCGAbiolinks:::getProjectSummary("TCGA-GBM")
+TCGAbiolinks:::getProjectSummary("TCGA-READ")
+TCGAbiolinks:::getProjectSummary("TCGA-COAD")
 
 # Search TCGA data
+
+# check for project types: https://bioconductor.org/packages/release/bioc/vignettes/TCGAbiolinks/inst/doc/query.html
 query <- GDCquery(
-  project = c("TCGA-GBM"),
+  project = c("TCGA-READ"),
   data.category = c("Simple Nucleotide Variation"),
   access = c("open"))
 
 # Download TCGA data 
-#GDCdownload(query)
+GDCdownload(query)
 
 # Prepare data in SummarizedExperiment (default data structure for TCGAbiolinks) format. It can be saved as an R object.
 ## Mutation data is MAF files. I think these can not be saved as SummarizedExperiment.
 
-#data = GDCprepare(query,
-#                  summarizedExperiment = TRUE,
-#                  save = TRUE,
-#                  save.filename = gsub(":", "_", paste0(query$project, date(), "GDCprepare.rda"))) 
+data = GDCprepare(query,
+                  summarizedExperiment = TRUE,
+                  save = TRUE,
+                  save.filename = gsub("-", "", paste0(query$project,'_',Sys.Date(), "_GDCprepare.Rdata"))) 
 
 
 # Load GDC prepare file as 'data'
-load("TCGA-GBMWed Oct 16 12_14_57 2024GDCprepare.Rdata")
+
+load('GDCprepare/TCGACOAD_20241030_GDCprepare.Rdata')
 
 # Check metadata
 #TODO: Problem with receiving the data in summarizedExperiment format. Without it, clinical data is not accessible with colData function.
 #colData(data)
+
+# Metadata preparation
 
 barcode_list_for_metadata = unique(data$Tumor_Sample_Barcode)
 metadata = as.data.frame(colDataPrepare(barcode_list_for_metadata))
 metadata$barcode = gsub("-", ".", metadata$barcode) # using hyphens or dash sign (in general special characters) are not allowed in variable names. 
 rownames(metadata) = metadata$barcode
 # Write metadata
-#saveRDS(metadata, file = gsub(":", "_", paste0("metadata/",query$project, date(), "_metadata.rds")))
+saveRDS(metadata, file = gsub("-", "", paste0(query$project,'_',Sys.Date(), "_metadata.rds")))
 
 # Read maf file
 maf = data %>% maftools::read.maf()
@@ -58,5 +64,5 @@ plotmafSummary(maf = maf, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE
 maf_binary = maf2matrix(data)
 
 # save as tsv
-#fwrite(maf_binary, gsub(":", "_", paste0("binarymatrix/",query$project, date(), "_binary_mat.tsv")), sep = "\t", row.names = TRUE)
+#fwrite(maf_binary, gsub("-", "", paste0("binarymatrix/",query$project, Sys.Date(), "_binarymat.tsv")), sep = "\t", row.names = TRUE)
 
